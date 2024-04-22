ab9813e68d75ebf19a05c40a8a41674451226132
Guillermo <guillermosdlg@gmail.com>
2024-04-22 17:35:48 -0600

    Función para enviar y recibir datos de la Raspberry Pi Pico.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import threading
+import time
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 import random
+import serial
 
+# Abrir el puerto donde está conectada la Raspberry
+
+#ser = serial.Serial(port='COM3', baudrate=10000)
 
 # Menú principal función crear el menú prinicipal
 def mainMenu(window_width, window_height, Logo):
+
+    root.title("Menú Principal")
+    
     containter = Canvas(root, width=f"{window_width}", height=f"{window_height}", background="#14223E", highlightbackground="#14223E")
     containter.pack()
     containter.create_image(620, 300, image=Logo)
 
-    playButton = Button(containter, font=("Champions", 35), text="JUGAR")
+    playButton = Button(containter, font=("Champions", 35), text="JUGAR", command=sendPico)
     playButton.place(x=520, y=500)
 
     aboutButton = Button(containter, font=("Champions", 35), text="ACERCA")
@@ -18,6 +27,20 @@ def mainMenu(window_width, window_height, Logo):
 
     verLabel = Label(containter, font=("Champions", 25), text="VERSIÓN 1.0", bg="#14223E", fg="#FFFFFF")
     verLabel.place(x=20, y=950)
+
+# def readPico():
+#     global ser, datos
+#     while True:
+#         data = ser.readline()
+#         if (data.decode("UTF-8").strip())[0] == '>':
+#             pass
+#         else:
+#             datos.add(data.decode("UTF-8").strip())
+#         print(datos)
+#
+# def sendPico():
+#         dato = "hola()\r\n"
+#         ser.write(dato.encode())
 
 root = Tk()
 
@@ -33,6 +56,15 @@ root.config(background="#14223E")
 openLogo = Image.open("assets/logo.png")
 Logo = ImageTk.PhotoImage(openLogo.resize((1000, 1000)))
 
+# Variables para el funcionamiento del juego
+
+datos = set()
+
+threadPicoRead = threading.Thread(target=readPico)
+threadPicoRead.start()
+
+#threadPicoSend = threading.Thread(target=sendPico)
+#threadPicoSend.start()
 
 mainMenu(window_width, window_height, Logo)
 
