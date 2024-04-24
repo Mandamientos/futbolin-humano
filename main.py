55bf120b7da606cc4916fdb401ec20b6b06971c1
Guillermo <guillermosdlg@gmail.com>
2024-04-24 10:38:30 -0600

    En proceso config inicial

--- main.py
+++ main.py
@@ -15,18 +15,38 @@ def mainMenu(window_width, window_height, Logo):
 
     root.title("Menú Principal")
     
-    containter = Canvas(root, width=f"{window_width}", height=f"{window_height}", background="#14223E", highlightbackground="#14223E")
-    containter.pack()
-    containter.create_image(620, 300, image=Logo)
+    container = Canvas(root, width=f"{window_width}", height=f"{window_height}", background="#14223E", highlightbackground="#14223E")
+    container.pack()
+    container.create_image(620, 300, image=Logo)
 
-    playButton = Button(containter, font=("Champions", 35), text="JUGAR", command=sendPico)
+    playButton = Button(container, font=("Champions", 35), text="JUGAR", command=lambda:[container.destroy(), initConfig(window_width, window_height)])
     playButton.place(x=520, y=500)
 
-    aboutButton = Button(containter, font=("Champions", 35), text="ACERCA")
+    aboutButton = Button(container, font=("Champions", 35), text="ACERCA")
     aboutButton.place(x=510, y=650)
 
-    verLabel = Label(containter, font=("Champions", 25), text="VERSIÓN 1.0", bg="#14223E", fg="#FFFFFF")
+    verLabel = Label(container, font=("Champions", 25), text="VERSIÓN 1.0", bg="#14223E", fg="#FFFFFF")
     verLabel.place(x=20, y=950)
+
+
+# Menú configuración inicial
+def initConfig(window_width, window_height):
+
+    global logoMCI, logoRMA
+
+    root.title("Configuración inicial")
+
+    container = Canvas(root, width=f"{window_width}", height=f"{window_height}", background="#14223E", highlightbackground="#14223E")
+    container.pack()
+
+    verLabel = Label(container, font=("Champions", 40), text="CONFIGURACIÓN INICIAL", bg="#14223E", fg="#FFFFFF")
+    verLabel.place(x=350, y=100)
+
+    MCIButton = Button(container, image=logoMCI, background="#14223E")
+    MCIButton.place(x=100, y=400)
+
+    RMAButton = Button(container, image=logoRMA, background="#14223E")
+    RMAButton.place(x=100, y=400)
 
 # def readPico():
 #     global ser, datos
@@ -56,12 +76,18 @@ root.config(background="#14223E")
 openLogo = Image.open("assets/logo.png")
 Logo = ImageTk.PhotoImage(openLogo.resize((1000, 1000)))
 
+logoMCIOp = Image.open("assets/LogoMCI.png")
+logoMCI = ImageTk.PhotoImage(logoMCIOp.resize((250, 250)))
+
+logoRMAOp = Image.open("assets/LogoRMA.png")
+logoRMA = ImageTk.PhotoImage(logoRMAOp.resize((250, 250)))
+
 # Variables para el funcionamiento del juego
 
 datos = set()
 
-threadPicoRead = threading.Thread(target=readPico)
-threadPicoRead.start()
+#threadPicoRead = threading.Thread(target=readPico)
+#threadPicoRead.start()
 
 #threadPicoSend = threading.Thread(target=sendPico)
 #threadPicoSend.start()
