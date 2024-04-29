db3eeb9b7b8a6047e3252094577d98ca7efc91f7
Guillermo <guillermosdlg@gmail.com>
2024-04-29 15:56:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -39,14 +39,43 @@ def initConfig(window_width, window_height):
     container = Canvas(root, width=f"{window_width}", height=f"{window_height}", background="#14223E", highlightbackground="#14223E")
     container.pack()
 
-    verLabel = Label(container, font=("Champions", 40), text="CONFIGURACIÓN INICIAL", bg="#14223E", fg="#FFFFFF")
-    verLabel.place(x=350, y=100)
+    configTitle = Label(container, font=("Champions", 40), text="CONFIGURACIÓN INICIAL", bg="#14223E", fg="#FFFFFF")
+    configTitle.place(x=30, y=30)
 
-    MCIButton = Button(container, image=logoMCI, background="#14223E")
+    pickTitle = Label(container, font=("ITC Novarese Std Medium", 30), text="ESCOJA DOS EQUIPOS", bg="#14223E", fg="#FFFFFF")
+    pickTitle.place(x=380, y=250)
+
+    MCIButton = Button(container, image=logoMCI, background="#14223E", state="normal", command=lambda:[choosenTeams(MCIButton, "Manchester City", container)])
     MCIButton.place(x=100, y=400)
 
-    RMAButton = Button(container, image=logoRMA, background="#14223E")
-    RMAButton.place(x=100, y=400)
+    RMAButton = Button(container, image=logoRMA, background="#14223E", width=250, state="normal", command=lambda:[choosenTeams(RMAButton, "Real Madrid", container)])
+    RMAButton.place(x=450, y=400)
+
+    BARButton = Button(container, image=logoBAR, background="#14223E", width=250, state="normal", command=lambda:[choosenTeams(BARButton, "Barcelona FC", container)])
+    BARButton.place(x=800, y=400)
+
+
+# Guarda qué equipos escogió el usuario.
+def choosenTeams(button, teamName, container):
+    global firstTeam, secondTeam
+    if firstTeam == None:
+        button["state"] = "disabled"
+        firstTeam = f"{teamName}"
+    elif secondTeam == None:
+        button["state"] = "disabled"
+        secondTeam = f"{teamName}"
+        startButton = Button(container, font=("Champions", 40), text="SIGUIENTE", command=lambda:[container.destroy(), decideSides()]).place(x=440, y=800)
+    else:
+        pass
+
+# Decide quien es el local y quien el visitante
+def decideSides():
+    global local, visiting
+    teams = [firstTeam, secondTeam]
+    local = random.choice(teams)
+    del teams[teams.index(local)]
+    visiting = teams[0]
+    print("Local:", local, "\nVisitante:", visiting)
 
 # def readPico():
 #     global ser, datos
@@ -80,11 +109,18 @@ logoMCIOp = Image.open("assets/LogoMCI.png")
 logoMCI = ImageTk.PhotoImage(logoMCIOp.resize((250, 250)))
 
 logoRMAOp = Image.open("assets/LogoRMA.png")
-logoRMA = ImageTk.PhotoImage(logoRMAOp.resize((250, 250)))
+logoRMA = ImageTk.PhotoImage(logoRMAOp.resize((200, 250)))
+
+logoBAROp = Image.open("assets/logoBAR.png")
+logoBAR = ImageTk.PhotoImage(logoBAROp.resize((250, 250)))
 
 # Variables para el funcionamiento del juego
 
 datos = set()
+firstTeam = None
+secondTeam = None
+local = None
+visiting = None
 
 #threadPicoRead = threading.Thread(target=readPico)
 #threadPicoRead.start()
